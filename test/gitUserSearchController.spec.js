a310503a163151f5852f4c7ea489e25a68bc1359
Jongmin Kim <kjm141215@gmail.com>
2015-10-21 08:36:26 +0100

    Add controller module & search factory

---
+++ test/gitUserSearchController.spec.js
@@ -0,0 +1,48 @@
+describe('GitUserSearchController', function() {
+  beforeEach(module('GitUserSearch'));
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('GitUserSearchController');
+  }))
+
+  it ('initializes with an empty search result and term', function() {
+    expect(ctrl.searchResult).toBeUndefined();
+    expect(ctrl.searchTerm).toBeUndefined();
+  })
+  describe('when searching for a user', function() {
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ]
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+        .expectGET("https://api.github.com/search/users?" + "access_token=" + access_token + "&q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
+
+    afterEach(function() {
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it ('displays search results', function() {
+      ctrl.searchTerm = 'hello';
+      ctrl.doSearch()
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    })
+  })
+})
