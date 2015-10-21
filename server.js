8c185ccd20e5bcec07a095c93aef6965840fbad6
Jongmin Kim <kjm141215@gmail.com>
2015-10-21 19:49:54 +0100

    Add Procfile and change server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var root = __dirname;
+var port = process.env.PORT || 8080;
 app.use(express.static(root + '/public/javascripts'));
 app.use(express.static(root));
 app.get('/', function(req, res) {
   res.sendFile(root + '/index.html');
 })
 
-var server = app.listen(8080, function() {
+var server = app.listen(port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
