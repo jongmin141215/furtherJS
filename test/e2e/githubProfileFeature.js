f00f81b132756ca2270fed4fcf09e50f7ad6b7fb
Jongmin Kim <kjm141215@gmail.com>
2015-10-21 19:20:27 +0100

    Add protractor and feature tests

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,26 @@
 describe('Github Profile finder', function() {
-  it ('has a title', function() {
-    browser.get('http://localhost:8080');
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
 
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
+  it ('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
+
+  it ('finds profiles', function() {
+    searchBox.sendKeys('spike0');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'))
+    expect(profiles.get(0).getText()).toEqual('spike01');
+  })
+
+  it ('finds the last Spike', function() {
+    searchBox.sendKeys('spike01');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'))
+    expect(profiles.last(0).getText()).toEqual('spike01');
+  })
 });
